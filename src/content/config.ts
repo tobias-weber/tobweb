3ff284e6c6a912148a531ba42af17b63fab192ed
Tobias Weber <tm.weber@stud.unibas.ch>
2025-05-23 01:54:59 +0200

    Introduce dark mode and basic pages

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// 1. Import utilities from `astro:content`
+import { defineCollection, z } from 'astro:content';
+
+// 2. Import loader(s)
+import { glob, file } from 'astro/loaders';
+
+// 3. Define your collection(s)
+const projects = defineCollection({
+    schema: z.object({
+        title: z.string(),
+        //description: z.string(),
+        pubDate: z.coerce.date(),
+        //updatedDate: z.coerce.date().optional(),
+    })
+});
+
+// 4. Export a single `collections` object to register your collection(s)
+export const collections = { projects };
