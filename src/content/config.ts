42a3ad9453aacc04d02fb1ad11b8f2ffaac2d13a
Tobias Weber <tm.weber@stud.unibas.ch>
2025-06-03 22:32:04 +0200

    Add master-thesis project

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const projects = defineCollection({
-    loader: glob({ base: './src/content/projects', pattern: '**/*.md' }),
+    loader: glob({ base: './src/content/projects', pattern: ['**/*.md', '**/*.mdx'] }),
     schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string(),
