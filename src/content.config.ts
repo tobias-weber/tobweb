7f8d40c78e2d50fdc3d4e8010d7d305cc34df5fc
Tobias Weber <tm.weber@stud.unibas.ch>
2026-07-07 15:45:15 +0200

    Update packages

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,5 @@
-import { defineCollection, z } from 'astro:content';
-
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
 import { glob } from 'astro/loaders';
 
 const projects = defineCollection({
